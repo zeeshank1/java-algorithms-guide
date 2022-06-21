e7344c8d9360f1b658e6e791bcefed6a5d783244
Zeeshan Khan <zeeshank1@gmail.com>
2022-06-22 00:07:45 +0530

    UPDATE : naive pattern search algo

--- src/main/java/algorithms/pattern_searching/NaivePatternSearching.java
+++ src/main/java/algorithms/pattern_searching/NaivePatternSearching.java
@@ -2,7 +2,7 @@ package algorithms.pattern_searching;
 
 public class NaivePatternSearching {
 
-    private static boolean naivePatternSearch(String textString, String pattern) {
+    private static int naivePatternSearch(String textString, String pattern) {
 
         int textStringLength = textString.length();
         int patternLength = pattern.length();
@@ -11,7 +11,7 @@ public class NaivePatternSearching {
 
             char currentTextChar = textString.charAt(i);
             char firstPatternChar = pattern.charAt(0);
-            System.out.println("CharAt Text i > "+i +" = "+ currentTextChar);
+            System.out.println("CharAt Text i > " + i + " = " + currentTextChar);
             System.out.println("First CharAt Pattern = " + currentTextChar);
             System.out.println("========================================");
             if (currentTextChar == firstPatternChar) {
@@ -19,24 +19,22 @@ public class NaivePatternSearching {
                     char t = textString.charAt(i + j);
                     char p = pattern.charAt(j);
                     System.out.println("T = " + t + " ___ " + "P = " + p);
-                    if (t == p) {
-                        System.out.println("In IF");
-                    } else {
+                    if (t != p) {
                         break;
                     }
                     if (j == patternLength - 1) {
-                        return true;
+                        return i;
                     }
                 }
             }
         }
 
-        return false;
+        return -1;
     }
 
     public static void main(String[] args) {
         String textString = "AABACCBBAAFF";
-        String pattern = "BBA";
-        System.out.println("Pattern Found : " + naivePatternSearch(textString, pattern));
+        String pattern = "FF";
+        System.out.println("Pattern Found at index : " + naivePatternSearch(textString, pattern));
     }
 }
